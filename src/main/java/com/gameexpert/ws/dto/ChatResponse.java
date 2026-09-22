361567d40100104c2fba7b1544481f7ad58e84a9
qkrwovlf98 <wovlf2wl@naver.com>
2026-09-22 18:27:05 +0900

    Lv13. 채팅 요청 처리와 응답 구성

--- src/main/java/com/gameexpert/ws/dto/ChatResponse.java
+++ src/main/java/com/gameexpert/ws/dto/ChatResponse.java
@@ -5,7 +5,14 @@ import lombok.Getter;
 
 @Getter
 public class ChatResponse {
+    String type="chat";
+    String sender;
+    String content;
+    LocalDateTime timestamp;
     // TODO Lv 13: API 명세에 맞게 응답 필드와 생성자를 완성합니다.
-    public ChatResponse(String sender, String content, LocalDateTime timestamp) {
+    public ChatResponse( String sender, String content, LocalDateTime timestamp) {
+        this.sender=sender;
+        this.content=content;
+        this.timestamp=timestamp;
     }
 }
