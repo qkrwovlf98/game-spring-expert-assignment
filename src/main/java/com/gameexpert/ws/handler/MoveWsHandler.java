2b3cc6cc95cbab30b98f1d53a10193bfe0e74d6f
qkrwovlf98 <wovlf2wl@naver.com>
2026-09-22 17:45:49 +0900

    Lv12. 플레이어 이동 요청 처리

--- src/main/java/com/gameexpert/ws/handler/MoveWsHandler.java
+++ src/main/java/com/gameexpert/ws/handler/MoveWsHandler.java
@@ -21,5 +21,17 @@ public class MoveWsHandler implements WsMessageHandler {
     public void handle(WsMessageContext context, JsonNode message) {
         String finalSceneActionId = WsFields.optionalFinalSceneActionId(message);
         // TODO Lv 12: 명세의 이동 값을 읽어 현재 사용자의 이동 요청을 엔진에 전달합니다.
+        PlayerAction playerAction = new PlayerAction.Move(
+                context.nickname(),
+                WsFields.finiteNumber(message,"x"),
+                WsFields.finiteNumber(message,"y"),
+                WsFields.finiteNumber(message,"z"),
+                WsFields.finiteFloat(message,"yaw"),
+                WsFields.finiteFloat(message,"pitch"),
+                WsFields.booleanValue(message,"crouching"),
+                WsFields.booleanValue(message,"gliding"),
+                finalSceneActionId
+        );
+        engineManager.enqueue(context.worldId(), playerAction);
     }
 }
