af0b76a56c35ed889dae9ee48fe36c605c41d197
qkrwovlf98 <wovlf2wl@naver.com>
2026-09-22 19:48:45 +0900

    Lv 15. 접속자 목록 조회

--- src/main/java/com/gameexpert/ws/dto/OnlineUsersResponse.java
+++ src/main/java/com/gameexpert/ws/dto/OnlineUsersResponse.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 
 @Getter
 public class OnlineUsersResponse {
+    String type = "onlineUsers";
+    List<String> users;
+    int count;
     // TODO Lv 15: API 명세에 맞게 응답 필드와 생성자를 완성합니다.
     public OnlineUsersResponse(List<String> users, int count) {
+        this.users=users;
+        this.count=count;
     }
 }
--- src/main/java/com/gameexpert/ws/handler/OnlineUsersWsHandler.java
+++ src/main/java/com/gameexpert/ws/handler/OnlineUsersWsHandler.java
@@ -1,5 +1,6 @@
 package com.gameexpert.ws.handler;
 
+import java.util.ArrayList;
 import java.util.List;
 import com.gameexpert.ws.NicknameHandshakeInterceptor;
 import com.gameexpert.ws.WorldBroadcaster;
@@ -8,6 +9,7 @@ import com.gameexpert.ws.WsMessageContext;
 import com.gameexpert.ws.dto.OnlineUsersResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
 import tools.jackson.databind.JsonNode;
 
 @Component
@@ -24,5 +26,15 @@ public class OnlineUsersWsHandler implements WsMessageHandler {
     @Override
     public void handle(WsMessageContext context, JsonNode message) {
         // TODO Lv 15: 현재 월드의 열린 연결에서 닉네임을 조회하고 요청자에게 응답합니다.
+        List<String> onlineUsers=new ArrayList<>();
+        for(var x : registry.entries(context.worldId())){
+            if(x.session().isOpen()){
+                onlineUsers.add((String)x.session().getAttributes()
+                        .get(NicknameHandshakeInterceptor.ATTR_NICKNAME));
+            }
+        }
+        onlineUsers.sort(String::compareTo);
+        OnlineUsersResponse onlineUsersResponse = new OnlineUsersResponse(onlineUsers,onlineUsers.size());
+        broadcaster.sendTo(context.session(), onlineUsersResponse);
     }
 }
